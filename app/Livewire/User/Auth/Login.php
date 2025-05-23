290f694e07da49e7aa849385f911640ca884fb1c
Denis Augusto <denisgusto@gmail.com>
2025-05-23 15:42:09 -0300

    TP-01: login user successfully

--- app/Livewire/User/Auth/Login.php
+++ app/Livewire/User/Auth/Login.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\User\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Login extends Component
 {
+    public ?string $email;
+
+    public ?string $password;
+
+    public function submit()
+    {
+        if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            return;
+        }
+
+        return $this->redirectRoute('welcome');
+    }
+
     public function render()
     {
         return view('livewire.user.auth.login');
--- tests/Feature/Livewire/User/Auth/LoginTest.php
+++ tests/Feature/Livewire/User/Auth/LoginTest.php
@@ -1,9 +1,27 @@
 <?php
 
 use App\Livewire\User\Auth\Login;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Login::class)
         ->assertStatus(200);
+});
+
+it('should be able to user login', function () {
+    $user = User::factory()->create([
+        'email'    => 'joe@doe.com',
+        'password' => 'password',
+    ]);
+
+    Livewire::test(Login::class)
+        ->set('email', 'joe@doe.com')
+        ->set('password', 'password')
+        ->call('submit')
+        ->assertHasNoErrors()
+        ->assertRedirectToRoute('welcome');
+
+    expect(auth()->check())->toBeTrue()
+        ->and(auth()->user())->id->toBe($user->id);
 });
