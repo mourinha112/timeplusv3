6129fcf63be3bfc29a87749cf1b210d617b85a07
Denis Augusto <denisgusto@gmail.com>
2025-05-27 23:20:47 -0300

    add reasons

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GenderSeeder::class,
             SpecialtySeeder::class,
+            ReasonSeeder::class,
         ]);
     }
 }
