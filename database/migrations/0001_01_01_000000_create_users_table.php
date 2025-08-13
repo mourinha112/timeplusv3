767f6a6527eb3a7c71ad63250c9822f4e4d710f9
Thiago Garcia <thiago.garcia@tagee.tech>
2025-08-13 15:54:59 -0300

    add gateway_customer_id field to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,11 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('gateway_customer_id')->nullable()->unique();
 
             $table->text('avatar')->nullable();
             $table->string('name');
