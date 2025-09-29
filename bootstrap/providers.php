61c85ba87069e3ea4d76ed7fe308ab0d8662377c
Denis Augusto <denisgusto@gmail.com>
2025-09-29 16:48:41 -0300

    add telescope for local and production

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\PagarmeServiceProvider::class,
     App\Providers\SendGridServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
