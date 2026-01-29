a780bafe45586285d78d88d66a0df8dded47f277
Anderson <mourinha113@gmail.com>
2026-01-29 11:11:17 -0300

    envio de email)a]

--- app/Notifications/EmployeeCredentialsNotification.php
+++ app/Notifications/EmployeeCredentialsNotification.php
@@ -2,14 +2,11 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EmployeeCredentialsNotification extends Notification implements ShouldQueue
+class EmployeeCredentialsNotification extends Notification
 {
-    use Queueable;
 
     public function __construct(
         public string $companyName,
